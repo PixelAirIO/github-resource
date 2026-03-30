e4f5efdbbcbc72e842d8e999901adcf7ac2b528f
Taylor Silva <dev@taydev.net>
2026-03-30 15:26:47 -0400

    implement pr/check step
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- kinds/pr/check.go
+++ kinds/pr/check.go
@@ -1,5 +1,62 @@
 package pr
 
-func (p *Pr) Check(stdin []byte) {
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
 
+	gh "github.com/PixelAirIO/github-resource"
+)
+
+type checkRequest struct {
+	Source  Source  `json:"source"`
+	Version version `json:"version"`
+}
+
+func (*Pr) Check(stdin []byte) {
+	dc := json.NewDecoder(bytes.NewReader(stdin))
+	dc.DisallowUnknownFields()
+
+	var request checkRequest
+	err := dc.Decode(&request)
+	if err != nil {
+		log.Fatalf("failed to unmarshal check request: %v", err)
+	}
+
+	err = validateSource(&request.Source)
+	if err != nil {
+		log.Fatalf("validation error: %v", err)
+	}
+
+	ghc, err := gh.NewGithubClient(request.Source.Config.Config)
+	if err != nil {
+		log.Fatalf("failed to create Github client: %v", err)
+	}
+
+	newVersion := check(request, ghc)
+
+	out, err := json.Marshal(newVersion)
+	if err != nil {
+		log.Fatalf("failed to marshal output: %v", err)
+	}
+
+	fmt.Println(string(out))
+}
+
+func check(request checkRequest, ghc gh.GithubClient) []version {
+	sha, err := ghc.LatestCommitForPR(request.Source.Number)
+	if err != nil {
+		log.Fatalf("failed to get latest commit for PR: %v", err)
+	}
+
+	if request.Version.SHA == sha {
+		return nil
+	}
+
+	return []version{
+		version{
+			SHA: sha,
+		},
+	}
 }
--- kinds/pr/types.go
+++ kinds/pr/types.go
@@ -1,6 +1,8 @@
 package pr
 
 import (
+	"errors"
+
 	gh "github.com/PixelAirIO/github-resource"
 )
 
@@ -15,8 +17,17 @@ type Source struct {
 
 type Config struct {
 	gh.Config
-	CommitSHA   string `json:"commit_sha"`
-	Name        string `json:"name"`
-	Status      string `json:"status"`
-	Description string `json:"description"`
+	Number int `json:"number"`
+}
+
+type version struct {
+	SHA string `json:"sha"`
+}
+
+func validateSource(src *Source) (err error) {
+	if src.Number == 0 {
+		err = errors.Join(errors.New("'number' field is required and should be set to the PR's number"), err)
+	}
+
+	return err
 }
